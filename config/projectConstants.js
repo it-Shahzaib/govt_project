b89f8e3b61232dd4b27d20c39fdeae03a7bcee33
Shahzaib Ahmed <shahzaib.thirtynorth@gmail.com>
2024-01-31 12:12:28 +0500

    Added project constants

---
+++ config/projectConstants.js
@@ -0,0 +1,11 @@
+const projectTypes = Object.freeze({
+  GOP: "GOP",
+  Foreign_Funded: "Foreign Funded",
+});
+
+const projectTypesArray = Object.values(projectTypes);
+
+module.exports = {
+  projectTypes,
+  projectTypesArray,
+};
