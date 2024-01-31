b2349e97bbee54567799188d968352a74a0bb3f0
Shahzaib Ahmed <shahzaib.thirtynorth@gmail.com>
2024-01-31 13:04:46 +0500

    Added readProjectService helper function

--- services/projectService.js
+++ services/projectService.js
@@ -11,73 +11,13 @@ const projectService = async (project_id) => {
       },
       {
         $lookup: {
-          from: "projectcontainers",
-          let: { projectId: "$_id" },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $in: ["$$projectId", "$projects"],
-                },
-              },
-            },
-          ],
-          as: "related_project_container",
-        },
-      },
-      {
-        $unwind: {
-          path: "$related_project_container",
-        },
-      },
-      {
-        $lookup: {
           from: "activities",
           let: { projectId: "$_id" },
           pipeline: [
             {
               $match: {
                 $expr: {
-                  $eq: ["$$projectId", "$project_id"],
-                },
-              },
-            },
-            {
-              $lookup: {
-                from: "items",
-                let: { activityIds: "$_id" },
-                pipeline: [
-                  {
-                    $match: {
-                      $expr: {
-                        $eq: ["$$activityIds", "$activity_id"],
-                      },
-                    },
-                  },
-                ],
-                as: "related_activity_items",
-              },
-            },
-            {
-              $lookup: {
-                from: "bills",
-                let: { activityId: "$_id" },
-                pipeline: [
-                  {
-                    $match: {
-                      $expr: {
-                        $eq: ["$$activityId", "$activity_id"],
-                      },
-                    },
-                  },
-                ],
-                as: "related_activity_bills",
-              },
-            },
-            {
-              $addFields: {
-                related_bills_cost: {
-                  $sum: "$related_activity_bills.total",
+                  $eq: ["$project_id", "$$projectId"],
                 },
               },
             },
@@ -86,74 +26,17 @@ const projectService = async (project_id) => {
         },
       },
       {
-        $lookup: {
-          from: "projectreleases",
-          let: { projectId: "$_id" },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $eq: ["$$projectId", "$project_id"],
-                },
-              },
-            },
-          ],
-          as: "related_project_releases",
-        },
-      },
-      {
         $unwind: {
-          path: "$related_project_releases",
+          path: "$related_activities",
           preserveNullAndEmptyArrays: true,
         },
       },
       {
-        $lookup: {
-          from: "activityreleases",
-          let: { projectId: "$_id" },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $eq: ["$$projectId", "$project_id"],
-                },
-              },
-            },
-          ],
-          as: "related_activities_releases",
-        },
-      },
-      {
-        $project: {
-          name: {
-            $concat: ["$related_project_container.name", "-", "$type"],
-          },
-          related_activities: "$related_activities",
-          projectReleases: "$related_projects_releases",
-          activityReleases: "$related_activities_releases",
-          approved_cost_total: {
-            $sum: ["$approved_cost_foreign_funded", "$approved_cost_gop"],
-          },
-          approved_cost_related_to_type: {
-            $cond: {
-              if: { $eq: ["$type", "GOP"] },
-              then: "$approved_cost_gop",
-              else: "$approved_cost_foreign_funded",
-            },
-          },
+        $group: {
+          _id: null,
           cost_used_by_activities: {
             $sum: "$related_activities.amount",
           },
-          released_amount: {
-            $sum: "$related_project_releases.release_amount",
-          },
-          remaining_budget: {
-            $subtract: [
-              { $sum: "$related_project_releases.release_amount" },
-              { $sum: "$related_activities.related_bills_cost" },
-            ],
-          },
-          _id: 0,
         },
       },
     ];
@@ -161,7 +44,7 @@ const projectService = async (project_id) => {
     const result = await projectModel.aggregate(pipeline);
 
     if (result.length > 0) {
-      return result;
+      return result[0].cost_used_by_activities;
     } else {
       return null;
     }
